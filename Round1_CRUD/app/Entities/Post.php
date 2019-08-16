63ffc0f40b5e209760b6644f20da234ebc4b46c4
Shiva <shivaxsin@gmail.com>
2019-08-16 14:46:01 +0800

    mode (Post) & (User) function relevance

--- Round1_CRUD/app/Entities/Post.php
+++ Round1_CRUD/app/Entities/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [                     // $fillable 內都是允許「被填入值」的欄位
+        'user_id', 'title', 'content',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
